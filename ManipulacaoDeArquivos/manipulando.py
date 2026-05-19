aab8c8d92d31fc72b2750c24c79faf0129666751
Arthur Saraiva <arthursaraiva.dev@gmail.com>
2026-05-19 12:19:18 -0300

    Manipulando Arquivos

---
+++ ManipulacaoDeArquivos/manipulando.py
@@ -0,0 +1,20 @@
+import os
+
+
+arquivo1 = open("dados1.txt", 'w', encoding='utf-8') #ABRE O ARQUIVO EM MODO WRITE
+#CASO NÃO HAJA ARQUIVO, ELE CRIARÁ AUTOMATICAMENTE
+print("Caminho Absoluto ARQUIVO 1: " + os.path.abspath(arquivo1.name)) #PRINTA O CAMINHO ABSOLUTO DO ARQUIVO
+
+arquivo2 = open("C:/Users/Arthur/Documents/GitHub/Python-Studies/ManipulacaoDeArquivos/dados2.txt", 'w', encoding='utf-8')
+#ABRINDO/CRIANDO ARQUIVO ESPECIFICANDO O CAMINHO
+
+
+arquivo1.write("Hello World!")
+arquivo2.write("Olá Mundo!")
+
+print("Caminho Real Arquivo 1: " + os.path.realpath(arquivo1.name))
+print("Caminho Real Arquivo 2: " + os.path.realpath(arquivo2.name))
+print(arquivo1)
+
+arquivo1.close
+arquivo2.close
